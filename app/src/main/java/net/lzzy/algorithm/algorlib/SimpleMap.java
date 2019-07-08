f9be285315fbfc0ab1dbd522c735745097abd0be
zck0663 <1501045682@qq.com>
2019-07-08 18:30:24 +0800

    最短路程

--- app/src/main/java/net/lzzy/algorithm/algorlib/SimpleMap.java
+++ app/src/main/java/net/lzzy/algorithm/algorlib/SimpleMap.java
@@ -1,7 +1,9 @@
 package net.lzzy.algorithm.algorlib;
 
 import java.util.ArrayList;
+import java.util.LinkedList;
 import java.util.List;
+import java.util.Queue;
 
 /**
  * Created by lzzy_gxy on 2019/6/27.
@@ -10,8 +12,9 @@ import java.util.List;
 public class SimpleMap {
     int vertexCount;
     List<Edge> edges = new ArrayList<>();
+    List<Integer> visited;
 
-    public SimpleMap (int v) {
+    public SimpleMap(int v) {
         vertexCount = v;
     }
 
@@ -19,36 +22,102 @@ public class SimpleMap {
         edges.add(new Edge(source, target, distance));
     }
 
-    public void addTwoWayEdge(int v1, int v2, double distance) {
-        addEdge(v1, v2, distance);
-        addEdge(v2, v1, distance);
+    public void addTwoWayEdge(int v1, int v2, double d1) {
+        addEdge(v1, v2, d1);
+        addEdge(v2, v1, d1);
     }
 
-    public String iterateDepthFirst() {
-        return "015234";
+    List<Edge> getConnectedEdges(int source) {
+        List<Edge> result = new ArrayList<>();
+        for (Edge e : edges) {
+            if (e.getSource() == source) {
+                result.add(e);
+            }
+        }
+        return result;
+    }
+
+    private void iterateDepth(int v) {
+        if (!visited.contains(v)) {
+            visited.add(v);
+        }
+        if (visited.size() == vertexCount) {
+            return;
+        }
+        List<Edge> vEdges = getConnectedEdges(v);
+        for (Edge edge : vEdges) {
+            if (visited.contains(edge.getTarget())) {
+                continue;
+            }
+            iterateDepth(edge.getTarget());
+        }
+    }
+
+    public String iterateDEpthFirst() {
+        visited = new ArrayList<>();
+        iterateDepth(0);
+        StringBuilder result = new StringBuilder();
+        for (int v : visited) {
+            result.append(v).append(",");
+        }
+        return result.toString();
     }
 
     public String iterateRangeFirst() {
-        return "012354";
+//        return "012354";
+        StringBuilder result = new StringBuilder();
+        Queue<Integer> queue = new LinkedList<>();
+        queue.offer(0);
+        int head = 0, tail = 1;
+        while (head < tail) {
+            Integer cursor = queue.poll();
+            if (cursor == null) {
+                break;
+            }
+            result.append(cursor).append(",");
+            List<Edge> vEdges = getConnectedEdges(cursor);
+            for (Edge edge : vEdges) {
+                if (!queue.contains(edge.getTarget())) {
+                    queue.offer(edge.getTarget());
+                    tail++;
+                }
+                if (tail > vertexCount) {
+                    break;
+                }
+            }
+            head++;
+        }
+        while (queue.size() > 0) {
+            result.append(queue.poll()).append(",");
+        }
+        return result.toString();
+
     }
+
 }
 
-class Edge{
+class Edge {
     private int source;
     private int target;
     private double distance;
-    int getSource() {
-        return source;
-    }
-    int getTarget() {
-        return target;
-    }
-    double getDistance() {
-        return distance;
-    }
+
     Edge(int source, int target, double distance) {
+        this.distance = distance;
         this.source = source;
         this.target = target;
-        this.distance = target;
     }
+
+    public int getSource() {
+        return source;
+    }
+
+    public double getDistance() {
+        return distance;
+    }
+
+    public int getTarget() {
+        return target;
+    }
+
 }
+
